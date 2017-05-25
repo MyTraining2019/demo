b0c4c0f0a9252c7b490062fd313d39723a2b73b9
Training2017 <captrainingfeb2016@gmail.com>
2017-05-25 13:47:26 +0530

    First Commit

---
+++ BankApp/src/test/java/org/cap/bankapp/test/MyFirstTest.java
@@ -0,0 +1,16 @@
+package org.cap.bankapp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class MyFirstTest {
+
+	@Category(GoodTestCategory.class)
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
